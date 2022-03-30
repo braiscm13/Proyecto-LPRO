4f38e4dbaabe7f5a1d447b77a53d02057df910bc
braiscm13 <spyro.brais@gmail.com>
2022-03-30 23:24:17 +0200

    casi con qr

--- TEL/Agular/turnos/src/app/mainpage/mainpage.component.ts
+++ TEL/Agular/turnos/src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 
@@ -17,15 +17,16 @@ export class MainpageComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private consultasService: ConsultasService,
-    private location: Location
+    private location: Location,
+    private router: Router,
   ) {}
 
 
   turno: any={};
 
-  turno1: any;
-  turno2: any;
-  turno3: any;
+  turno1: String='';
+  turno2: String='';
+  turno3: String='';
 
   colas:  String ='';
   cola1=false;
@@ -159,12 +160,150 @@ export class MainpageComponent implements OnInit {
    }
 
 
-      //setTimeout(() => {  console.log(this.turno); }, 10); Con esto podemos hacer que espere lo suficiente como para que acabe la consulta
-
 
 
 
   }
+
+
+  createTurnQR(){
+
+
+    if(this.cola1 && this.cola2 && this.cola3){
+ 
+     console.log("Colas 1, 2 y 3");
+ 
+     this.colas="123";
+       
+       this.consultasService.newTurn(this.colas).subscribe((turnos: any) =>{
+         console.log(turnos);
+         this.turno=turnos.vuelta;
+         this.turno1= this.turno.res[0];
+         this.turno2= this.turno.res[1];
+         this.turno3= this.turno.res[2];
+         //console.log(this.turno1+this.turno2+this.turno3);
+ 
+ 
+     }
+     );
+ 
+    }
+ 
+    if(this.cola1 && !this.cola2 && !this.cola3){
+ 
+     console.log("Cola 1");
+ 
+     this.colas="1";
+       
+       this.consultasService.newTurn(this.colas).subscribe((turnos: any) =>{
+         console.log(turnos);
+         this.turno=turnos.vuelta;
+         this.turno1= this.turno.res;
+ 
+ 
+     }
+     );
+ 
+    }
+ 
+ 
+    if(!this.cola1 && this.cola2 && !this.cola3){
+ 
+     console.log("Cola 2");
+ 
+     this.colas="2";
+       
+       this.consultasService.newTurn(this.colas).subscribe((turnos: any) =>{
+         console.log(turnos);
+         this.turno=turnos.vuelta;
+         this.turno2= this.turno.res;
+ 
+     }
+     );
+ 
+    }
+ 
+    if(!this.cola1 && !this.cola2 && this.cola3){
+ 
+     console.log("Cola 3");
+ 
+     this.colas="3";
+       
+       this.consultasService.newTurn(this.colas).subscribe((turnos: any) =>{
+         console.log(turnos);
+         this.turno=turnos.vuelta;
+         this.turno3= this.turno.res;
+ 
+     }
+     );
+ 
+    }
+ 
+    if(this.cola1 && this.cola2 && !this.cola3){
+ 
+     console.log("Colas 1 y 2");
+ 
+     this.colas="12";
+       
+       this.consultasService.newTurn(this.colas).subscribe((turnos: any) =>{
+         console.log(turnos);
+         this.turno=turnos.vuelta;
+         this.turno1= this.turno.res[0];
+         this.turno2= this.turno.res[1];
+ 
+     }
+     );
+ 
+    }
+ 
+    if(this.cola1 && !this.cola2 && this.cola3){
+ 
+     console.log("Cola 1 y 3");
+ 
+     this.colas="13";
+       
+       this.consultasService.newTurn(this.colas).subscribe((turnos: any) =>{
+         console.log(turnos);
+         this.turno=turnos.vuelta;
+         this.turno1= this.turno.res[0];
+         this.turno3= this.turno.res[1];
+ 
+     }
+     );
+ 
+    }
+ 
+    if(!this.cola1 && this.cola2 && this.cola3){
+ 
+     console.log("Colas 2 y 3");
+ 
+     this.colas="23";
+       
+       this.consultasService.newTurn(this.colas).subscribe((turnos: any) =>{
+         console.log(turnos);
+         this.turno=turnos.vuelta;
+         this.turno2= this.turno.res[0];
+         this.turno3= this.turno.res[1];
+ 
+     }
+     );
+ 
+    }
+
+
+ 
+ 
+       setTimeout(() => {  
+         console.log("Hola "+ this.turno1);
+         console.log("Hola "+ this.turno2);
+         console.log("Hola "+ this.turno3);
+         this.router.navigate(['/qr', this.turno1, this.turno2, this.turno3]); 
+        }, 1000); //Con esto podemos hacer que espere lo suficiente como para que acabe la consulta
+ 
+ 
+ 
+ 
+   }
 
 
   ngOnInit(): void {
